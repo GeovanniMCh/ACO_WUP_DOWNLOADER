0dfa7a8b7024e3c2f962f0c5751acdac82924155
GeovanniMCh <42387370+GeovanniMCh@users.noreply.github.com>
2018-10-12 17:23:07 -0600

    Add files via upload

---
+++ themes.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+# If you want to play with predefined colors you can find them all here:
+# http://www.science.smith.edu/dftwiki/index.php/File:TkInterColorCharts.png
+
+themes = {
+    
+"Dark": {
+    "main": "black",
+    "maintext": "light grey",
+    "box": "black",
+    "boxtext": "light grey"},
+
+"Medium": {
+    "main": "gray30",
+    "maintext": "white",
+    "box": "grey",
+    "boxtext": "black"},
+
+"Light": {
+    "main": "gray94",
+    "maintext": "black",
+    "box": "white",
+    "boxtext": "black"},
+
+"Beautiful": {
+    "main": "medium purple4",
+    "maintext": "hot pink1",
+    "box": "gray2",
+    "boxtext": "hot pink2"},
+
+"Cyber": {
+    "main": "gray1",
+    "maintext": "springgreen4",
+    "box": "palegreen4",
+    "boxtext": "gray1"},
+
+"Powershell": {
+    "main": "#012456",
+    "maintext": "gray91",
+    "box": "#012456",
+    "boxtext": "gray91"},
+
+"Royal": {
+    "main": "gray18",
+    "maintext": "gray84",
+    "box": "slategray2",
+    "boxtext": "gray1"},
+
+"Royal 2": {
+    "main": "gray18",
+    "maintext": "slategray2",
+    "box": "slategray4",
+    "boxtext": "black"}
+
+}
+ 
+
+        
+            
+
+    
