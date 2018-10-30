fee430ffb42280f2ee283e3024a76fd4d4946d3d
GeovanniMCh <42387370+GeovanniMCh@users.noreply.github.com>
2018-10-29 19:46:36 -0600

    Add files via upload

--- themes.py
+++ themes.py
@@ -11,6 +11,47 @@ themes = {
     "box": "black",
     "boxtext": "light grey"},
 
+"Medium": {
+    "main": "gray30",
+    "maintext": "white",
+    "box": "grey",
+    "boxtext": "black"},
+
+"Light": {
+    "main": "gray94",
+    "maintext": "black",
+    "box": "white",
+    "boxtext": "black"},
+
+"Beautiful": {
+    "main": "medium purple4",
+    "maintext": "hot pink1",
+    "box": "gray2",
+    "boxtext": "hot pink2"},
+
+"Cyber": {
+    "main": "gray1",
+    "maintext": "springgreen4",
+    "box": "palegreen4",
+    "boxtext": "gray1"},
+
+"Powershell": {
+    "main": "#012456",
+    "maintext": "gray91",
+    "box": "#012456",
+    "boxtext": "gray91"},
+
+"Royal": {
+    "main": "gray18",
+    "maintext": "gray84",
+    "box": "slategray2",
+    "boxtext": "gray1"},
+
+"Royal 2": {
+    "main": "gray18",
+    "maintext": "slategray2",
+    "box": "slategray4",
+    "boxtext": "black"}
 
 }
  
